6050a8fe4399795f30ffd551a61ef2c1fbff492b
Christian Fobel <christian@fobel.net>
2012-08-14 12:19:18 -0400

    Add docstrings to k_scatter/k_gather

--- scatter_gather.py
+++ scatter_gather.py
@@ -4,11 +4,41 @@ import numpy as np
 
 
 def k_scatter(data, scatter_lists):
+    '''
+    Given a 1D array of values, which we'll call "data", and an ordered set
+    of k-sized arrays (which we'll call "scatter_lists") of indices into
+    "data", generate an ordered set of arrays, where each array corresponds
+    to the "data" values for a particular list of indices from
+    "scatter_lists".
+
+    For example (note that here k=2, i.e., the scatter lists are of size 2):
+
+    >>> data = [1, 3, 0, 2, 5, 4]
+    >>> scatter_lists = [[0, 3], [1, 4], [0, 5]]
+    >>> scattered_data = k_scatter(data, scatter_lists)
+    >>> scattered_data
+    [[1, 2], [3, 5], [1, 4]]
+    '''
+    stride = len(scatter_lists[0])
+    for i, d in enumerate(scatter_lists[1:]):
+        assert(len(d) == stride)
     return [[data[i] for i in scatter_list]
             for scatter_list in scatter_lists]
 
 
 def k_gather(scattered_data):
+    '''
+    Given a list of k-sized arrays (potentially the output of k-scatter),
+    concatenate them into a single array.
+
+    For example (note that here k=2, i.e., the scatter data arrays are of
+    size 2):
+
+    >>> scattered_data = [[1, 2], [3, 5], [1, 4]]
+    >>> gathered_data = k_gather(scattered_data)
+    >>> gathered_data
+    array([1, 2, 3, 5, 1, 4])
+    '''
     stride = len(scattered_data[0])
     for i, d in enumerate(scattered_data[1:]):
         assert(len(d) == stride)
