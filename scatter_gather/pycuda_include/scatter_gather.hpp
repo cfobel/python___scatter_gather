ed3d10e46265edbafe71d43482809042e325f15e
Christian Fobel <christian@fobel.net>
2012-08-16 12:11:20 -0400

    Refactor k_scatter() into ScatterManager class

--- scatter_gather/pycuda_include/scatter_gather.hpp
+++ scatter_gather/pycuda_include/scatter_gather.hpp
@@ -30,71 +30,77 @@ namespace scatter_gather {
     }
 
 
-    /*
-     * Interpret entry `e` in scatter_lists[] with the value `empty_index`
-     * as an indication that the corresponding entry in the `block_data` array
-     * should be set to the value `empty_value`.
-     *
-     * e.g., Consider the following:
-     *
-     *         k = 2
-     *         data = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-     *         scatter_lists = [[4, -1], [3, 7], [-1, 2]]
-     *         scatter_count = 3
-     *
-     *     Note that some of the entries in `scatter_lists` have a value of -1.
-     *     This is the default `empty_index` value.  Therefore, in the
-     *     `block_data` array, any entries corresponding to these -1 indices
-     *     should be set to `empty_value` (in this case `empty_value` is zero).
-     *     The resulting `block_data` array is then:
-     *
-     *         block_data = [7, 0, 8, 4, 0, 9]
-     */
-    template <class T, int empty_index=-1>
-    __device__ void k_scatter(int k, int data_count, volatile T *data,
-            int scatter_count, int32_t *scatter_lists, volatile T *block_data,
-                    T empty_value) {
-#ifdef DEBUG_SCATTER_GATHER
-        if(threadIdx.x == 0 && blockIdx.x == 0) {
-            printf("k_scatter(k=%d, scatter_count=%d)\n", k, scatter_count);
+    template <class T, int _empty_index=-1>
+    class ScatterManager {
+    public:
+        int _data_count;
+        volatile T *_data;
+        T _empty_value;
+
+        __device__ ScatterManager(int data_count, volatile T *data, T empty_value)
+                : _data_count(data_count), _data(data), _empty_value(empty_value) {
         }
+
+        /*
+        * Overload `_empty_value` to zero.  Note that this will cause a
+        * compilation error if 0 cannot be implicitly casted to type
+        * `T`.
+        */
+        __device__ ScatterManager(int data_count, volatile T *data) : _data_count(
+                data_count), _data(data), _empty_value(0) {}
+
+        /*
+        * Interpret entry `e` in scatter_lists[] with the value `_empty_index`
+        * as an indication that the corresponding entry in the `block_data` array
+        * should be set to the value `_empty_value`.
+        *
+        * e.g., Consider the following:
+        *
+        *         k = 2
+        *         data = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+        *         scatter_lists = [[4, -1], [3, 7], [-1, 2]]
+        *         scatter_count = 3
+        *
+        *     Note that some of the entries in `scatter_lists` have a value of -1.
+        *     This is the default `_empty_index` value.  Therefore, in the
+        *     `block_data` array, any entries corresponding to these -1 indices
+        *     should be set to `_empty_value` (in this case `_empty_value` is zero).
+        *     The resulting `block_data` array is then:
+        *
+        *         block_data = [7, 0, 8, 4, 0, 9]
+        */
+        __device__ void scatter(int k, int scatter_count,
+                int32_t *scatter_lists, volatile T *block_data) {
+#ifdef DEBUG_SCATTER_GATHER
+            if(threadIdx.x == 0 && blockIdx.x == 0) {
+                printf("k_scatter(k=%d, scatter_count=%d)\n", k, scatter_count);
+            }
 #endif
-        int passes = ceil((float)scatter_count / blockDim.x);
-        for(int j = 0; j < passes; j++) {
-            int i = j * blockDim.x + threadIdx.x;
-            if(i < scatter_count) {
-                for(int scatter_index = 0; scatter_index < k; scatter_index++) {
-                    int index = scatter_lists[i * k + scatter_index];
-                    if(index == empty_index) {
-                        block_data[threadIdx.x * k + scatter_index] = empty_value;
-                    } else {
-                        T value = data[index];
-                        block_data[threadIdx.x * k + scatter_index] = value;
+            int passes = ceil((float)scatter_count / blockDim.x);
+            for(int j = 0; j < passes; j++) {
+                int i = j * blockDim.x + threadIdx.x;
+                if(i < scatter_count) {
+                    for(int scatter_index = 0; scatter_index < k; scatter_index++) {
+                        int index = scatter_lists[i * k + scatter_index];
+                        if(index == _empty_index) {
+                            block_data[threadIdx.x * k + scatter_index] = _empty_value;
+                        } else {
+                            T value = _data[index];
+                            block_data[threadIdx.x * k + scatter_index] = value;
 
 #ifdef DEBUG_SCATTER_GATHER
-                        printf("{'block_id': %d, 'thread_id': %d, 'pass': %d, 'element_id': %d, 'value': %d}\n",
-                                blockIdx.x, threadIdx.x, j, i, value);
-                        if(index >= data_count) {
-                            printf("Out of bounds: %d/%d\n", index, data_count);
-                        }
+                            printf("{'block_id': %d, 'thread_id': %d, 'pass': %d, 'element_id': %d, 'value': %d}\n",
+                                    blockIdx.x, threadIdx.x, j, i, value);
+                            if(index >= _data_count) {
+                                printf("Out of bounds: %d/%d\n", index, _data_count);
+                            }
 #endif
+                        }
                     }
                 }
             }
         }
-    }
-
-
-    /*
-     * Overload k_scatter function to default the `empty_value` to zero.  Note
-     * that this will cause a compilation error if 0 cannot be implicitly
-     * casted to type `T`.
-     */
-    template <class T, int empty_index=-1>
-    __device__ void k_scatter(int k, int data_count, volatile T *data,
-            int scatter_count, int32_t *scatter_lists, volatile T *block_data) {
-        k_scatter<T, empty_index>(k, data_count, data, scatter_count, scatter_lists, block_data, 0);
-    }
+    };
 
 
     template <class T>
--- scatter_gather/pycuda_templates/scatter_gather.cu
+++ scatter_gather/pycuda_templates/scatter_gather.cu
@@ -21,8 +21,8 @@ extern "C" __global__ void k_scatter_{{ c_type }}(int k, int data_count,
     #endif
     {{ c_type }} *block_data = ({{ c_type }} *)&shared_data[0];
 
-    scatter_gather::k_scatter<{{ c_type }}>(k, data_count, data, scatter_count,
-            scatter_lists, &block_data[0]);
+    scatter_gather::ScatterManager<{{ c_type }}> scatter_manager(data_count, data);
+    scatter_manager.scatter(k, scatter_count, scatter_lists, &block_data[0]);
     //scatter_gather::dump_data(scatter_count * k, &block_data[0]);
     scatter_gather::k_gather<{{ c_type }}>(k, scatter_count, &block_data[0], gathered_data);
 
