Fix shared memory size calculation,...
Simplify computation of global_index in CUDA k_scatter_global function.
The global_index is used to index into the scatter_list_order array.

scatter_gather/cuda.py
@@ -74,11 +74,22 @@ def scatter_gather(in_data, scatter_lists, scatter_list_order=None, dtype=None,
 
     block = (thread_count, 1, 1)
     grid = (block_count, 1, 1)
-    shared = int(scatter_count * k) * dtype.type(0).itemsize
+
+    # The number of scatter lists to be processed by all thread blocks
+    # (except the first, in the case where scatter_count does not divide
+    # evenly by block_count)
+    common_scatter_count = scatter_count // block_count
+
+    # If scatter_count does not divide evenly by block_count, compute
+    # how many extra elements must be processed by the first thread block
+    odd_scatter_count = scatter_count % block_count
+
+    shared = int(np.ceil((common_scatter_count + odd_scatter_count) * k)
+            ) * dtype.type(0).itemsize
 
     print 'thread_count: %d' % thread_count
     print 'block_count: %d' % block_count
-    
+    print 'shared mem/block: %d' % shared
 
     if scatter_list_order is None:
         scatter_list_order = np.arange(len(scatter_lists), dtype=np.uint32)

tests/__init__.py
@@ -53,13 +53,14 @@ def test_k_scatter_gather():
 
 
 def test_k_scatter_cuda():
-    data = np.arange(29, dtype=np.int32)[::-1].copy()
+    data = np.arange(400, dtype=np.int32)[::-1].copy()
     data_count = np.int32(len(data))
     rng = np.random.RandomState()
     rng.seed(0)
-    for two_power in range(13):
+    for two_power in range(15):
         scatter_list_count = 1 << two_power
-        scatter_lists = [rng.randint(data_count, size=3) for i in range(scatter_list_count)]
+        scatter_lists = [rng.randint(data_count, size=3)
+                for i in range(scatter_list_count)]
         scatter_lists[0][-1] = -2
         scatter_lists[-1][-1] = -2
 
@@ -69,21 +70,7 @@ def test_k_scatter_cuda():
         gathered_data_cpu = k_gather(scatter_manager.k_scatter(scatter_lists,
                 scatter_list_order))
 
-        gathered_data_cuda = scatter_gather(data, scatter_lists, scatter_list_order)
-                #block_count=?, thread_count=256)
+        gathered_data_cuda = scatter_gather(data, scatter_lists,
+                scatter_list_order)
 
-        to_print = (('data_id', np.arange(len(data))),
-                ('data', data), ('scatter_lists', scatter_lists),
-                ('scatter_list_order', scatter_list_order),
-                ('gathered_data (CPU)', gathered_data_cpu),
-                ('gathered_data (CUDA)', gathered_data_cuda))
-        max_label_length = max(len(label) for label, d in to_print)
-        format_str = '%%%ds' % (max_label_length + 1)
-
-        #for label, data_ in to_print:
-            #print format_str % label, data_
-
-        #try:
         all_close(gathered_data_cpu, gathered_data_cuda, ['CPU', 'CUDA'])
-        #except:
-            #import pudb; pudb.set_trace()